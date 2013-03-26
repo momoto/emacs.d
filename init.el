d372ac409c90be9169327c73f5df27610772dbee
Seiji Momoto <seiji.momoto@facebook.com>
2013-03-26 21:02:05 +0900

    Added "delete-auto-save-files"

--- init.el
+++ init.el
@@ -1,17 +1,23 @@
 
-(setq-default c-basic-offset 2      ;;基本インデント量
-              tab-width 2           ;;タブ幅
-              indent-tabs-mode nil) ;;インデントをタブでするかスペースでするか
+;; メニューバー非表示
+(menu-bar-mode -1)
+;; ツールバー非表示
+(tool-bar-mode -1)
 
-(setq c-auto-newline nil) ;; 全自動インデントを無効
+(setq-default tab-width 2           ;; タブ幅
+              indent-tabs-mode nil) ;; t => インデントにタブを使用する nil => スペースを使用する
 
-;; read uim.el with LEIM initializing
+
+;; インプットメソッドを指定する
 ;; (require 'uim-leim)
-
 (require 'mozc)
-(setq default-input-method "japanese-mozc")
-
-;; set default IM. Uncomment the one of the followings.
 ;(setq default-input-method "japanese-anthy-utf8-uim")	; Anthy (UTF-8)
 ;(setq default-input-method "japanese-mozc-uim")	; Mozc
+(setq default-input-method "japanese-mozc")
 (setq mozc-candidate-style 'overlay)
+
+
+;; Auto-savingの無効化
+;; (setq make-backup-files nil)
+;; 保存が完了した時にAuto-saveファイルを消す
+(setq delete-auto-save-files t)
