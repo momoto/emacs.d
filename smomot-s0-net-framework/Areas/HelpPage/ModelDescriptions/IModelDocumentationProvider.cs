11739fe5d45e8b90680b05ed411f0df8f72cacc9
百戸 誠二 <seiji.momoto@jbs.com>
2019-06-10 14:41:11 +0900

    プロジェクト ファイルを追加します。

---
+++ smomot-s0-net-framework/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace smomot_s0_net_framework.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ smomot-s0-net-framework/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace smomot_s0_net_framework.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
