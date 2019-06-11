8c1610e4a4c6b63a7d146a876bce5923e02520c8
百戸 誠二 <seiji.momoto@jbs.com>
2019-06-11 12:46:04 +0900

    XML ドキュメンテーションコメントを有効化した

--- smomot-s0-net-framework/Controllers/ValuesController.cs
+++ smomot-s0-net-framework/Controllers/ValuesController.cs
@@ -7,35 +7,44 @@ using System.Web.Http;
 
 namespace smomot_s0_net_framework.Controllers
 {
+    /// <summary>
+    /// Value(s) を応答する
+    /// </summary>
     public class ValuesController : ApiController
     {
-        // GET api/values
+        /// <summary>
+        /// Value(s) のリストを応答する
+        /// </summary>
         public IEnumerable<string> Get()
         {
             System.Diagnostics.Trace.TraceInformation("カスタムメッセージです");
             System.Diagnostics.Trace.TraceWarning("カスタムメッセージです");
-            System.Diagnostics.Trace.TraceError("カスタムメッセージです");
+            // System.Diagnostics.Trace.TraceError("カスタムメッセージです");
 
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5
+        /// <summary>
+        /// 特定の {id} に対応する Value を応答する
+        /// </summary>
         public string Get(int id)
         {
             return "value";
         }
-
-        // POST api/values
+        /// <summary>
+        /// Value を登録する
+        /// </summary>
         public void Post([FromBody]string value)
         {
         }
-
-        // PUT api/values/5
+        /// <summary>
+        /// 特定の {id} の Value を変更する
+        /// </summary>
         public void Put(int id, [FromBody]string value)
         {
         }
-
-        // DELETE api/values/5
+        /// <summary>
+        /// 特定の {id} の Value を削除する
+        /// </summary>
         public void Delete(int id)
         {
         }
