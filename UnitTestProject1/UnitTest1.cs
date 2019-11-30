86fda9ad7fc0592f3ceb7f5174b524f4d856b3ac
百戸 誠二 <seiji.momoto@jbs.com>
2019-12-01 00:28:16 +0900

    Add Console.WriteLine() to output strings.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ConsoleApp2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1, "yay :)");
+        }
+    }
+}
